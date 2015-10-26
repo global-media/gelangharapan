f18037b52d5777fcf1a5f1aa80335b011f05a14d
Jaffer Sadeg <jaffersadeg@gmail.com>
2015-10-26 21:48:40 +0700

    fix wording

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -24,9 +24,9 @@ module PagesHelper
     str << "<h3><i>People Behind</i><b>HOPE</b></h3><p>"
     
     str << if english?
-            "It all started with a dream and small hope..."
+            "It all started with a dream and a 'little hope' of a couple of best friends who wants to create solidarity and the culture of giving amongst the people and to spread Hope in all aspects of life through selling bracelets. The bracelet is made from recycled materials taken from senior Indonesian designer Ghea Panggabean which is Kain Pelangi Jumputan. 'Jumputan' means Rainbow and therefore simbolyzes HOPE. That is how Bracelet of HOPE was created. For the kick off of the movement, we started by spreading HOPE and awareness about cancer for cancer patients and their families.\n In the years ahead, Bracelet of Hope is commited to spreading HOPE in all aspects of Life."
           else
-            "Bermula dari mimpi dan suatu 'harapan' kecil dari beberapa sahabat di bulan Oktober 2014 yang ingin membangkitkan solidaritas dan budaya menyumbang diantara masyarakat dan menyebarkan harapan di segala aspek dalam kehidupan melalui sebuah gelang yang dibuat dari sisa kain designer senior kebanggaan Indonesia, Ghea Panggabean yaitu Kain Pelangi Jumputan, dan Pelangi adalah simbol dari Harapan. Terciptalah Gelang Harapan #BraceletofHOPE yang kemudian menjadi suatu 'Harapan Besar' dan Kenyataan karena energi positif dan sambutan dari semua relawan dan 'Warriors of Hope'(Pejuang Harapan).Gerakkan @gelangharapan berkomitmen untuk terus menyebarkan \"Harapan\", berawal khususnya kepada pejuang kanker dan keluarganya, dan di tahun-tahun mendatang \"Harapan\" di segala aspek dalam Kehidupan."
+            "Bermula dari mimpi dan suatu 'harapan' kecil dari beberapa sahabat di bulan Oktober 2014 yang ingin membangkitkan solidaritas dan budaya menyumbang diantara masyarakat dan menyebarkan harapan di segala aspek dalam kehidupan melalui sebuah gelang yang dibuat dari sisa kain designer senior kebanggaan Indonesia, Ghea Panggabean yaitu Kain Pelangi Jumputan, dan Pelangi adalag simbol dari Harapan. Terciptalah Gelang Harapan #BraceletofHOPE yang kemudian menjadi suatu 'Harapan Besar' dan Kenyataan karena energi positif dan sambutan dari semua relawan dan 'Warriors of Hope'(Pejuang Harapan).\nGerakkan @gelangharapan berkomitmen untuk terus menyebarkan \"Harapan\", berawal khususnya kepada pejuang kanker dan keluarganya, dan di tahun-tahun mendatang \"Harapan\" di segala aspek dalam Kehidupan."
           end
           
     str << "</p></center>"
@@ -39,7 +39,7 @@ module PagesHelper
           when :jenna
             str = "<h3><b>janna</b>soekasah joesoef</h3><p>"
             str << if english?
-                    '"Hope is making dream comes true"'
+                    '"Hope is a way of making dreams into reality"'
                   else
                     '"Harapan adalah cara membuat mimpi menjadi kenyataan"'
                   end
@@ -47,7 +47,7 @@ module PagesHelper
           when :wulan
             str = "<h3><b>Wulan</b>Guritno</h3><p>"
             str << if english?
-                    '"Patience, All tribulation will come to end"'
+                    '"Hold On, Pain Ends"'
                   else
                     '"Bertahanlah, Segala Kesusahan akan Berakhir"'
                   end
@@ -55,9 +55,9 @@ module PagesHelper
           when :amanda
             str = "<h3><b>amanda</b>soekasah</h3><p>"
             str << if english?
-                    '"When you choose hope, everything..."'
+                    "\"Once you choose Hope, Everything becomes Possible\".\n HOPE is a Legacy and the journey that defines me...\nAs long as I live, and as long as i could, I will dedicate my LIFE on spreading HOPE"
                   else
-                    '"Saat Engkau memilih Harapan, semua yang kamu pikir tidak mungkin menjadi mungkin.<br>Hope adalah Legacy dan Perjalanan kami.  Selama saya hidup, saya akan terus berusaha untuk terus menyebarkan Harapan"'
+                    "\"Saat Engkau memilih Harapan, semua yang kamu pikir tidak mungkin menjadi mungkin.\n Hope adalah Legacy dan Perjalanan kami.  Selama saya hidup, saya akan terus berusaha untuk terus menyebarkan Harapan\""
                   end
             str << "</p>"
           end 
@@ -67,13 +67,13 @@ module PagesHelper
   
   def news_header
     str = '<center><img class="matahari img-responsive" src="'
-    str << asset_path('img/people/matahari.png')
+    str << asset_path('img/health/matahari.png')
     str << '">'
     str << '<h3><i>Hope Health</i> <b>NEWS</b></h3><p>'
     str << if english?
             "Bracelet of Hope movement working together with RSCM..."
           else
-            "Gerakkan Gelang Harapan bekerjasama dengan RSCM dan Ketua Komite Penanggulangan Kanker Nasional untuk dapat membantu dan memberikan info mengenai penyakit kanker dan masalah seputarnya. Semua Warrior of Hope mempunyai kesempatan untuk dapat langsung mengirimkan pertanyaan kepada Profesor Soehartati dan Team Dokter RSCM yang senantiasa mendedikasikan waktunya. Bersama-sama dengan Profesor Soehartati, Team Dokter RSCM dan atas bantuan seluruh Warriors of Hope, di bulan Oktober 2015 ini akan dibuka \"Room of Hope\", yang merupakan ruang tunggu pertama bagi pasien BPJS dan keluarganya yang selain memberikan Harapan dan Kebahagiaan, juga memberikan keterampilan untuk menolong segi sosial dari para pasien dan keluarganya (lihat upcoming events untuk cerita selengkapnya)."
+            "Gerakkan Gelang Harapan bekerjasama dengan RSCM dan Ketua Komite Penanggulangan Kanker Nasional untuk dapat membantu dan memberikan info mengenai penyakit kanker dan masalah seputarnya.\n Semua Warrior of Hope mempunyai kesempatan untuk dapat langsung mengirimkan pertanyaan kepada Profesor Soehartati dan Team Dokter RSCM yang senantiasa mendedikasikan waktunya.\n\nBersama-sama dengan Profesor Soehartati, Team Dokter RSCM dan atas bantuan seluruh Warriors of Hope, di bulan Oktober 2015 ini akan dibuka \"Room of Hope\", yang merupakan ruang tunggu pertama bagi pasien BPJS dan keluarganya yang selain memberikan Harapan dan Kebahagiaan, juga memberikan keterampilan untuk menolong segi sosial dari para pasien dan keluarganya (lihat upcoming events untuk cerita selengkapnya).\n\n Profile\n Prof. DR. Dr. Soehartati Gondhowiardjo, Sp.Rad (K) Onk.Rad \n Jabatan: Ketua Komite Penanggulangan Kanker Nasional \n\n Untuk Q& A: kankerindonesia@gmail.com"
           end
     str << '</p></center>'
     
@@ -84,7 +84,7 @@ module PagesHelper
     str = '<center><h3><i>Upcoming</i> <b>E V E N T S</b></h3><p>'
     
     str << if english? 
-              "We spread hope through..."
+              "We spread HOPE through all kinds of activation like free Mammografi, The Magic of HOPE for children with cancer and many more. 100% of the proceed of selling the bracelets are donated to cancer foundations in Indonesia and directly to cancer patients and their families through \"Journey of HOPE\"."
             else
               "Kami menyebarkan harapan/ hope melalui berbagai macam aktivasi seperti mamografi keliling, the 'magic of hope' untuk anak-anak dan banyak lagi. Hasil penjualan gelang kami salurkan setiap bulannya melaluu yayasan-yayasan kanker di Indonesia dan setiap 1-2 bulan kami turun langsung ke daerah-daerah untuk menyalurkan langsung melalui aktivasi yang kami sebut dengan Perjalanan Harapan (Journey of HOPE)."
             end
@@ -118,7 +118,7 @@ module PagesHelper
   def bracelet_header
     str = '<center><h3><i>Bracelet of </i><b>HOPE</b></h3><p>'
     str << if english?
-            "All sale will go towards Cancer Assocation..."
+            "The proceed of the sales from bracelet of HOPE will be donated to cancer foundations in Indonesia as well as unfortunate cancer patients and their families."
           else
             "Hasil penjualan gelang akan disalurkan ke Yayasan-Yayasan Kanker Di Indonesia dan langsung kepada orang- orang yang menderita kanker dan keluarganya melalui aktivasi-aktivasi kami."
           end
@@ -136,9 +136,9 @@ module PagesHelper
     str = '<center><div class="balon"></div>'
     str << '<h3><i>Share your Story of</i><b> HOPE</b></h3><p>'
     str << if english?
-            "This page contains a collection of viral campaign #IamHOPE, where..."
+            "In this page, we feature our viral campaign #IamHOPE where prominent public figures, celebrities and our warriors of HOPE can make a 15 seconds video about their Hopes for cancer or any other aspects in life and spread it through social media.\n You can also send your Hopes and stories of HOPE and help us spread HOPE,become a Warrior of HOPE and get chance to be featured in our web."
           else
-            "Page ini adalah kumpulan Viral campaign #IamHOPE dimana masyarakat/ public figure dan para pejuang Harapan dapat menyatakan harapannya di dalam video singkat 15 detik dan menyebarkannya di social media. Kirimkan juga harapan anda kepada kami di gelang harapan dengan #IamHOPE dan sebarkanlah harapan dan jadilah pejuang Harapan dan dapatkan kesempatan untuk ditampilkan di web kami."
+            "Page ini adalah kumpulan Viral campaign #IamHOPE dimana masyarakat/ public figure dan para pejuang Harapan dapat menyatakan harapannya di dalam video singkat 15 detik dan menyebarkannya di social media.\n Kirimkan juga harapan anda kepada kami di gelang harapan dengan #IamHOPE dan sebarkanlah harapan dan jadilah pejuang Harapan dan dapatkan kesempatan untuk ditampilkan di web kami."
             end
     str << '</p></center>'
     
@@ -188,6 +188,65 @@ module PagesHelper
     str << '</div>'
     
     raw(str)
+  end
+  
+  def story_videos_static
+    <<-EOF
+    <div class="col-md-4 col-xs-6 col-sm-4 artis" style="padding:0px">
+    <img class="img-responsive" data-target="#myModal" data-toggle="modal" src="#{asset_path 'img/Story/artis/wulan.jpg'}">
+       <video  class="video-1" width="100%" height="240" controls>
+      <source class="video-link1"  src="#{asset_path 'video/wulan.mp4'}" type="video/mp4">
+      </video>
+    </div>
+    <div class="col-md-4 col-xs-6 col-sm-4 artis" style="padding:0px">
+    <img class="img-responsive" data-target="#myModal" data-toggle="modal" src="#{asset_path 'img/Story/artis/1.jpg'}">
+       <video class="video-1"  width="100%" height="240" controls>
+      <source class="video-link1"   src="#{asset_path 'video/1.mp4'}" type="video/mp4">
+      </video>
+    </div>
+    <div class="col-md-4 col-xs-6 col-sm-4 artis" style="padding:0px">
+    <img class="img-responsive" data-target="#myModal" data-toggle="modal" src="#{asset_path 'img/Story/artis/2.jpg'}">
+       <video class="video-1"  width="100%" height="240" controls>
+      <source class="video-link1"   src="#{asset_path 'video/2.mp4'}" type="video/mp4">
+      </video>
+    </div>
+    <div class="col-md-4 col-xs-6 col-sm-4 artis" style="padding:0px">
+    <img class="img-responsive" data-target="#myModal" data-toggle="modal" src="#{asset_path 'img/Story/artis/3.jpg'}">
+       <video class="video-1"  width="100%" height="240" controls>
+      <source class="video-link1"   src="#{asset_path 'video/3.mp4'}" type="video/mp4">
+      </video>
+    </div>
+    <div class="col-md-4 col-xs-6 col-sm-4 artis" style="padding:0px">
+    <img class="img-responsive" data-target="#myModal" data-toggle="modal" src="#{asset_path 'img/Story/artis/4.jpg'}">
+       <video class="video-1"  width="100%" height="240" controls>
+      <source class="video-link1"   src="#{asset_path 'video/4.mp4'}" type="video/mp4">
+      </video>
+    </div>
+    <div class="col-md-4 col-xs-6 col-sm-4 artis" style="padding:0px">
+    <img class="img-responsive" data-target="#myModal" data-toggle="modal" src="#{asset_path 'img/Story/artis/5.jpg'}">
+       <video class="video-1"  width="100%" height="240" controls>
+      <source class="video-link1"   src="#{asset_path 'video/5.mp4'}" type="video/mp4">
+      </video>
+    </div>
+    <div class="col-md-4 col-xs-6 col-sm-4 artis" style="padding:0px">
+    <img class="img-responsive" data-target="#myModal" data-toggle="modal" src="#{asset_path 'img/Story/artis/6.jpg'}">
+       <video class="video-1"  width="100%" height="240" controls>
+      <source class="video-link1"   src="#{asset_path 'video/6.mp4'}" type="video/mp4">
+      </video>
+    </div>
+    <div class="col-md-4 col-xs-6 col-sm-4 artis" style="padding:0px">
+    <img class="img-responsive" data-target="#myModal" data-toggle="modal" src="#{asset_path 'img/Story/artis/7.jpg'}">
+       <video class="video-1"  width="100%" height="240" controls>
+      <source class="video-link1"   src="#{asset_path 'video/7.mp4'}" type="video/mp4">
+      </video>
+    </div>
+    <div class="col-md-4 col-xs-6 col-sm-4 artis" style="padding:0px">
+    <img class="img-responsive" data-target="#myModal" data-toggle="modal" src="#{asset_path 'img/Story/artis/8.jpg'}">
+       <video class="video-1"  width="100%" height="240" controls>
+      <source class="video-link1"   src="#{asset_path 'video/8.mp4'}" type="video/mp4">
+      </video>
+    </div>
+    EOF
   end
   
   def bracelet_items
