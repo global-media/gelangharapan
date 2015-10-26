a362c126c56b424f0e09a911d8bfb79ef04785e1
Jaffer Sadeg <jaffersadeg@gmail.com>
2015-10-26 14:27:03 +0700

    adding precomple assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,7 +9,7 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w( pages/css/login.css layout/css/custom.css pages/css/inbox.css layout/css/login.css layout/css/themes/darkblue.css )
-Rails.application.config.assets.precompile += %w( pages/css/profile.css pages/css/profile.css pages/css/tasks.css layout/css/layout.css css/todo.css layout/css/themes/darkblue.css layout/css/custom.css )
+Rails.application.config.assets.precompile += %w( pages/css/profile.css pages/css/profile.css pages/css/tasks.css layout/css/layout.css pages/css/todo.css layout/css/themes/darkblue.css layout/css/custom.css )
 # Rails.application.config.assets.precompile += %w( bower_components/bootstrap/dist/css/bootstrap.min.css bower_components/metisMenu/dist/metisMenu.min.css dist/css/timeline.css dist/css/sb-admin-2 bower_components/morrisjs/morris.css bower_components/font-awesome/css/font-awesome.min.css styles.css )
 # Rails.application.config.assets.precompile += %w( pages/js/jquery.js js/plugins.js js/bootstrap-datepicker.js )
 Rails.application.config.assets.precompile += %w( pages/css/bootstrap.css pages/style.css pages/css/dark.css pages/css/font-icons.css pages/css/animate.css pages/css/magnific-popup.css pages/css/responsive.css pages/css/datepicker.css)
@@ -17,3 +17,7 @@ Rails.application.config.assets.precompile += %w( img/logo.png )
 # Rails.application.config.assets.precompile += %w( bower_components/jquery/dist/jquery.min.js bower_components/bootstrap/dist/js/bootstrap.min.js bower_components/metisMenu/dist/metisMenu.min.js bower_components/raphael/raphael-min.js bower_components/morrisjs/morris.min.js js/morris-data.js dist/js/sb-admin-2.js )
 Rails.application.config.assets.precompile += %w( pages/scripts/components-pickers.js pages/scripts/components-form-tools2.js layout/scripts/layout.js layout/scripts/demo.js pages/scripts/login.js pages/scripts/contact-us.js )
 Rails.application.config.assets.precompile += %w( pages/scripts/todo.js layout/scripts/quick-sidebar.js pages/scripts/inbox.js pages/scripts/lock.js pages/scripts/profile.js pages/scripts/form-fileupload.js )
+Rails.application.config.assets.precompile += %w( pages/css/bootstrap.css pages/icon/font-awesome.css pages/icon/ionicons.css css/style.css pages/fonts/font.css pages/vendor/royalslider.css pages/vendor/skins/default/rs-default.css )
+Rails.application.config.assets.precompile += %w( pages/vendor/jquery-1.9.1.min.js pages/vendor/bootstrap.min.js pages/vendor/parallax.min.js pages/vendor/jquery.royalslider.min.js )
+Rails.application.config.assets.precompile += %w( pages/css/mobile.css pages/css/tablet-landscape.css pages/css/tablet-portrait.css pages/css/bootstrap.min.css pages/css/hope.css pages/css/font-awesome.css )
+Rails.application.config.assets.precompile += %w( pages/js/bootstrap.min.js )
