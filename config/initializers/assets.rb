8dbbdd0525b6314cabec4762c4c79ca97051fdf5
Jaffer Sadeg <jaffersadeg@gmail.com>
2015-10-26 18:58:49 +0700

    update font css assets path

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,7 +13,7 @@ Rails.application.config.assets.precompile += %w( pages/css/profile.css pages/cs
 # Rails.application.config.assets.precompile += %w( bower_components/bootstrap/dist/css/bootstrap.min.css bower_components/metisMenu/dist/metisMenu.min.css dist/css/timeline.css dist/css/sb-admin-2 bower_components/morrisjs/morris.css bower_components/font-awesome/css/font-awesome.min.css styles.css )
 # Rails.application.config.assets.precompile += %w( pages/js/jquery.js js/plugins.js js/bootstrap-datepicker.js )
 Rails.application.config.assets.precompile += %w( pages/css/bootstrap.css pages/style.css pages/css/dark.css pages/css/font-icons.css pages/css/animate.css pages/css/magnific-popup.css pages/css/responsive.css pages/css/datepicker.css)
-Rails.application.config.assets.precompile += %w( img/logo.png )
+Rails.application.config.assets.precompile += %w( img/logo.png img/loading.gif img/vimeo-play.png img/youtube-play.png )
 # Rails.application.config.assets.precompile += %w( bower_components/jquery/dist/jquery.min.js bower_components/bootstrap/dist/js/bootstrap.min.js bower_components/metisMenu/dist/metisMenu.min.js bower_components/raphael/raphael-min.js bower_components/morrisjs/morris.min.js js/morris-data.js dist/js/sb-admin-2.js )
 Rails.application.config.assets.precompile += %w( pages/scripts/components-pickers.js pages/scripts/components-form-tools2.js layout/scripts/layout.js layout/scripts/demo.js pages/scripts/login.js pages/scripts/contact-us.js )
 Rails.application.config.assets.precompile += %w( pages/scripts/todo.js layout/scripts/quick-sidebar.js pages/scripts/inbox.js pages/scripts/lock.js pages/scripts/profile.js pages/scripts/form-fileupload.js )
@@ -21,3 +21,7 @@ Rails.application.config.assets.precompile += %w( pages/css/bootstrap.css pages/
 Rails.application.config.assets.precompile += %w( pages/vendor/jquery-1.9.1.min.js pages/vendor/bootstrap.min.js pages/vendor/parallax.min.js pages/vendor/jquery.royalslider.min.js )
 Rails.application.config.assets.precompile += %w( css/mobile.css css/tablet-landscape.css css/tablet-portrait.css css/bootstrap.min.css css/hope.css css/font-awesome.css )
 Rails.application.config.assets.precompile += %w( js/bootstrap.min.js )
+Rails.application.config.assets.precompile += %w( fonts/glyphicons-halflings-regular.eot fonts/glyphicons-halflings-regular.woff2 fonts/glyphicons-halflings-regular.woff fonts/glyphicons-halflings-regular.ttf fonts/glyphicons-halflings-regular.svg )
+Rails.application.config.assets.precompile += %w( fonts/fontawesome-webfont.eot fonts/fontawesome-webfont.eot fonts/fontawesome-webfont.woff2 fonts/fontawesome-webfont.woff fonts/fontawesome-webfont.ttf fonts/fontawesome-webfont.svg )
+Rails.application.config.assets.precompile += %w( fonts/lg.eot fonts/lg.woff fonts/lg.ttf fonts/lg.svg )
+
